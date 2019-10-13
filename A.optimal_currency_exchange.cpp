83f36524b6d4b9d06e1d2e3b1729a866f3084ca7
jesuswr <41008968+jesuswr@users.noreply.github.com>
2019-10-12 20:22:05 -0400

    added solutions to 4 problems in a codeforces contest

---
+++ A.optimal_currency_exchange.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<int,pair<int,int>> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+#define ri(a) scanf("%d", &a);
+#define rii(a,b) scanf("%d %d", &a, &b);
+#define riii(a,b,c) scanf("%d %d %d", &a, &b, &c);
+#define rl(a) scanf("%lld", &a);
+#define rll(a,b) scanf("%lld %lld", &a, &b);
+
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 1e18;
+const int maxN = 1e5; // CAMBIAR ESTE
+
+// GJNM
+
+int main()
+{
+	ll n , d , e;
+	rl(n);
+	rll(d,e);
+	ll mn = n;
+	for (int i = 0; i*d <= n; ++i)
+	{
+		mn = min( mn , ( n - i*d)%(5*e));
+	}
+	printf("%lld\n", mn);
+
+	return 0;
+}
